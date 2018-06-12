a16f9b6e879836ff02c56a6299737cd3b6ed5f96
Justin Kenney <justinkenney13@gmail.com>
2018-06-12 18:18:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,11 +14,12 @@ class Artist
   end
 
   def new_song(name, genre)
-    name = Song.new(name, self, genre)    
+    name = Song.new(name, self, genre)
+    name.artist == self
   end
 
   def songs
-    
+    Song.all.select
   end
 
 
