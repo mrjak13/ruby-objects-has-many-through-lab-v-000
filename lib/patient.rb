aac05d5f0a3bd735c6b69a573485d48341c5ddd0
Justin Kenney <justinkenney13@gmail.com>
2018-06-12 18:42:40 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,15 @@
+class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self    
+  end
+
+  def new_appointment(doctor, date)
+    date = Appointment.new(date, self, doctor)    
+  end
+
+end
