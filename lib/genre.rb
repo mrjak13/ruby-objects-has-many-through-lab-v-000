ba400079fd8f0a1f6ee8cee056ae45104b8f14dd
Justin Kenney <justinkenney13@gmail.com>
2018-06-12 17:27:08 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,2 +1,26 @@
 class Genre
+  attr_accessor :name
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << name
+    @songs = []
+  end
+
+  def self.all
+    @@all    
+  end
+
+  def new_song(name, artist)
+    name = Song.new(name, artist, self)
+    songs << name
+    
+  end
+
+  def songs
+    @songs    
+  end
+  
 end
