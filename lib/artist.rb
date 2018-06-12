3a64448c6ea08742619db861179436a5307d014c
Justin Kenney <justinkenney13@gmail.com>
2018-06-12 18:27:21 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,8 +18,11 @@ class Artist
   end
 
   def songs
-    # binding.pry
     Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    songs.collect {|song| song.genre}    
   end
 
 
--- lib/genre.rb
+++ lib/genre.rb
@@ -19,7 +19,7 @@ class Genre
   end
 
   def songs
-    
+
   end
 
   def artists
